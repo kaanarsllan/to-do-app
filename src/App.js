0c339f2eef2aaa14726db689754ed478d1e63751
kaanarsllan <52761646+kaanarsllan@users.noreply.github.com>
2021-11-20 15:00:11 +0300

    todo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+
+const INITIAL_STATE = [{ id: 1, contents: "", tamamlandi: false}];
 
 function App() {
+  const [list, setList] = useState(INITIAL_STATE);
+  const [newAdd, setnewAdd] = useState("");
+  console.log("newadd", newAdd);
+
+  const addNew = (title) => {
+    setList([...list, { id: Date.now(), contents: newAdd, completed: false }]);
+    setnewAdd("");
+  };
+
+  const cleanCompleted = () => {
+    setList(list.filter((item) => !item.tamamlandi));
+  };
+  const markedTodo = (id) => {
+    setList(
+      list.map((e) => (e.id === id ? { ...e, tamamlandi: !e.tamamlandi } : e))
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1 className="head">To Do List</h1>
+      <div className="input">
+        <input
+          value={newAdd}
+          onChange={(e) => setnewAdd(e.target.value)}
+          placeholder="Please Insert To Do"
+        />
+        
+        <button className="addButton"
+          onClick={() => {
+            addNew(newAdd);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          
+          Add
+        </button>
+        <button onClick={() => cleanCompleted()} className="clean">
+          {" "}
+          Clear Completed To Do
+        </button>
+       </div>
+
+      <div className="list">
+        {list.map((item) => (
+          <div
+            key={item.id}
+            onClick={() => markedTodo(item.id)}
+            className={item.tamamlandi ? "yapildi " : ""}
+          >
+            {item.contents}
+          </div>
+        ))}
+        
+      </div>
     </div>
   );
 }
